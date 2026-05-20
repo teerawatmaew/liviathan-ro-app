1c15b5539eab0efacd75fe9e889aa3c5f6a4f5a1
teerawat.p <teerawat.p@chanwanich.digital>
2026-05-20 15:17:09 +0700

    Add - SpeedInsights for vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import { router } from '@/routes'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function App() {
   return (
     <>
       <RouterProvider router={router} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
